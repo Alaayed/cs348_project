7488a51514e409ec98ef4284eb7a96f909df36f6
saleh-alaayed <salaayed@purdue.edu>
2025-03-23 18:35:43 -0400

    Pings local db, created schemas, looking good.

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"backend/database"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"log"
 	"net/http"
 )
 
-func main() {
+func setUpDB() (*sql.DB, error) {
+	dsn := "salehAlaayed:1410@tcp(127.0.0.1:3306)/cs348_project"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	fmt.Println("Successfully connected to database!")
+	return db, nil
+}
+func setupRouter() (*mux.Router, *cors.Cors) {
 	router := mux.NewRouter()
 	if router == nil {
 		fmt.Println("Error creating router")
@@ -19,19 +36,34 @@ func main() {
 	router.HandleFunc("/test", testHandler).Methods("POST")
 
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{"http://localhost:2999"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
 	})
-
+	return router, c
+}
+func main() {
+	db, err := setUpDB()
+	if err != nil {
+		log.Fatal(err)
+		log.Println("Error setting up DB, crashing the whole system")
+		return
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(db)
+	router, c := setupRouter()
 	handler := c.Handler(router)
-
 	log.Println("Server is running on port 4000")
-	err := http.ListenAndServe(":4000", handler)
+	err = http.ListenAndServe(":4000", handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
+
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,7 +92,7 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	user := database.User{Id: 1}
 	if user.Id == 1 {
-		print(user)
+		print(user.Id)
 	}
 	return
 }
